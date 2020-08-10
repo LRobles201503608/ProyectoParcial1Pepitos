42030143a1beb351776578e0aa136099ab444169
201503608 <luisrobles2408@gmail.com>
2020-08-09 19:35:42 -0600

    añadiendo ramaPP

--- mvc/src/routes/index.js
+++ mvc/src/routes/index.js
@@ -14,6 +14,12 @@ router.get('/login',function(req,res,next){
     res.render('login.html');
 });
 
+router.post('/login2',(req,res,next)=>{
+    let entrada = req.nombre;
+    console.log(entrada);
+    res.send(JSON.stringify('bienvenido'));
+});
+
 router.post('/input',(req,res,next)=>{
     let entrada = req.body;
     console.log(entrada);
