058f5a4bec39a4fb434b548847791c2a950d6a6a
Frankin098 <iefranklin@hotmail.es>
2020-08-10 21:26:55 -0600

    Franklin

--- mvc/index.js
+++ mvc/index.js
@@ -1,25 +1,7 @@
-'use strict';
-const express=require('express');
+const app = require("./app.js");
 
-require('dotenv').config();
+const port = process.env.PORT || 3000;
 
-
-var logger= require('morgan');
-
-const port=process.env.PORT||3000;
-
-const path=require('path');
-
-var index= require('./src/routes/index');
-
-const app= express();
-
-app.use(logger('dev'));
-app.engine('html',require('ejs').renderFile);
-
-app.set('views',path.join(__dirname,'/src/views'));
-app.use('/',index);
-
-app.listen(port,function(){
-    console.log('servidor en el puerto: ',port);
+app.listen(port, function () {
+  console.log("servidor en el puerto: ", port);
 });
